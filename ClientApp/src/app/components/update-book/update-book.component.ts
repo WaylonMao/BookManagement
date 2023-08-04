f47c6c67762180a355d08bf260af9c7e230a221f
WaylonMao <tomcat_mao@hotmail.com>
2023-08-03 23:23:14 -0600

    Update book

--- ClientApp/src/app/components/update-book/update-book.component.ts
+++ ClientApp/src/app/components/update-book/update-book.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BookService} from 'src/app/services/book.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-update-book',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class UpdateBookComponent {
 
+  book: any;
+  updateBookForm!: FormGroup;
+
+  constructor(private service: BookService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.service.getBookById(this.route.snapshot.params.id).subscribe(data => {
+      this.book = data;
+      console.log('Data received:', data);
+      this.updateBookForm = this.fb.group({
+        id: [data.id],
+        title: [data.title, Validators.required],
+        author: [data.author, Validators.required],
+        description: [data.description, Validators.compose([Validators.required, Validators.minLength(30)])],
+        rate: [data.rate],
+        dateStart: [data.dateStart ? this.formatDate(data.dateStart) : null],
+        dateRead: [data.dateRead ? this.formatDate(data.dateRead) : null],
+      })
+      console.log('Form value:', this.updateBookForm.value);
+    })
+  }
+
+  formatDate(date: Date) {
+    if (date) {
+      return new Date(date).toISOString().substring(0, 10);
+    }
+    return null;
+  }
+
+  onSubmit() {
+    this.service.updateBook(this.updateBookForm.value).subscribe(data => {
+      this.router.navigate(["/books"]);
+    })
+  }
 }
